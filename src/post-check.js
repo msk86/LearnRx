52a6544de061b5faaa45ba171d44d22a7e3ceb88
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-07 17:27:13 +0800

    implement concatAll

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
 
 function postCheck() {
-
+    var afterConcatAll = [[1,2], [3,4], [5,6]].concatAll();
+    return `[[1,2],[3,4],[5,6]].concatAll() is [${afterConcatAll}]`;
 }
 
 export default postCheck;
