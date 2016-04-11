fe3024165b9e2a2829d8968841dabfa356028195
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 15:09:54 +0800

    Implement concatMap

--- src/post-check.js
+++ src/post-check.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
 
 function postCheck() {
-
+    let afterConcatMap = [1, 2].concatMap(x => [x, x]);
+    return `[1,2].concatMap(x => [x, x]) is [${afterConcatMap}]`;
 }
 
 export default postCheck;
--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,22 @@
-function exercise (console) {
+/*
+ Exercise 13: Implement concatMap
+    map and concatAll are very commonly chained together.
+    [1,2].concatMap(x => [x, x]]) is [1,1,2,2]
 
+
+    This concatMap function is also known as flatMap
+ */
+function exercise () {
+    Array.prototype.concatMap = function(projectionFunctionReturnArray) {
+        let sourceArray = this,
+            result = [];
+
+        /*
+         Add your code here
+         */
+
+        return result;
+    }
 }
 
 export default exercise;
