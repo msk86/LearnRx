89f7c79d4591d43a5c264d8574f55e4249e943e6
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-11 15:11:47 +0800

    add concatMap to prepare

--- src/prepare.js
+++ src/prepare.js
@@ -3,3 +3,7 @@ import _ from 'lodash'
 Array.prototype.concatAll = function() {
     return _.flatten(this);
 };
+
+Array.prototype.concatMap = function(projectionFunctionReturnsArray) {
+    return _.flatMap(this, projectionFunctionReturnsArray);
+};
