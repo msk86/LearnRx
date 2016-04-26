23918b1ba99ce1d091d8d8e8dabb859819df0fd9
Fan Wenbo <wbfan@thoughtworks.com>
2016-04-26 23:52:06 +0800

    takeUntil

--- src/your-code-here.js
+++ src/your-code-here.js
@@ -1,5 +1,20 @@
-function exercise (console) {
+/*
+ Exercise 30: Unsubscribe when another event fires.
+  Batman is fighting with his enemies from all super villains.
+  Of course, this time Batman has to fix his BatSuit. But when it need to be fixed, is an event.
 
+  For reference:
+   stream1 = stream([Villain,,,,,,Villain,,,,Villain,,,,,,,,.Villain,,,,])
+   stream2 = stream([,,,,,,,,,Smile,,,,,,,,Fix,,,,])
+    -> stream1.takeUntil(stream2) ->
+   result  = stream([Villain,,,,,,Villain])
+ */
+function exercise (superVillains, batSuitMaintaining, console) {
+    /*
+     Add your code here.
+     Try to use '.takeUntil(otherStream)'.
+     takeUntil function completes a stream when other event in otherStream occurs.
+     */
 }
 
 export default exercise;
