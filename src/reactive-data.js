0dd62d4fb03c7b0078dff50ce3f1725e2061b802
Fan Wenbo <wbfan@thoughtworks.com>
2016-05-03 11:12:08 +0800

    complete and incomplete

--- src/reactive-data.js
+++ src/reactive-data.js
@@ -1,12 +1,18 @@
+import _ from 'lodash'
 import Rx from 'rx'
 import data from './data'
 
 const dueTime = parseInt(Math.random() * 1000);
 const interval = parseInt(Math.random() * 500);
 const reactiveData = data.map(d => {
-    const timer = Rx.Observable.timer(dueTime, interval).take(d.length);
-    const rData = Rx.Observable.from(d);
-    return Rx.Observable.zip(timer, rData, (t, rd) => rd).filter(rd => rd);
+    const timer = Rx.Observable.timer(dueTime, interval);
+    let rData = timer.map(t => d[t]).filter(rd => rd);
+    if(typeof _.last(d) !== 'undefined') {
+        var valueCount = d.filter(v => v).length;
+        rData = rData.take(valueCount);
+    }
+
+    return rData;
 });
 
 export default reactiveData;
